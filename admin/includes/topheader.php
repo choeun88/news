94ccb59c6318803f30ea751acdd568d77ff3bb56
sreypovsum <52944272+sreypovsum@users.noreply.github.com>
2020-08-06 15:59:57 +0700

    udate hader

--- admin/includes/topheader.php
+++ admin/includes/topheader.php
@@ -1,42 +1,87 @@
-<div class="topbar">
-    <!-- LOGO -->
-    <div class="topbar-left">
-        <a href="index.html" class="logo"><span>NEWS<span>Top</span></span><i class="mdi mdi-layers"></i></a>
-        <!-- Image logo -->
-        <a href="index.html" class="logo">
-            <span>
-                <img src="assets/images/logo.png" alt="" height="30">
-            </span>
-            <!-- <i>
-                <img src="" alt="" height="28">
-            </i> -->
-        </a>
-    </div>
-    <!-- Button mobile view to collapse sidebar menu -->
-    <div class="navbar navbar-default" role="navigation">
-        <div class="container">
-            <ul class="nav navbar-nav navbar-left">
-                <li>
-                    <button class="button-menu-mobile open-left waves-effect">
+<div class="main-wrapper">
+   <header class="tobar" style="position: fixed; width: 100%; z-index: 50;transition: .2s ease-in; padding-left: 220px;">
+      <nav class="navbar top-navbar navbar-expand-md navbar-dark" style="background-color: #adadad; height: 62px;">
+         <!-- <a class="navbar-brand" href="#">
+            <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+            </a> -->
+         <div class="navbar-header" style="padding-right: 15px;line-height: 65px;padding-left: 15px;min-width: 70px;border-right: 1px solid rgba(0,0,0,.1);">
+            <div class="" role="navigation">
+               <div>
+                  <ul class="nav navbar-nav navbar-left">
+                     <li>
+                        <button class="button-menu-mobile open-left waves-effect">
                         <i class="mdi mdi-menu"></i>
-                    </button>
-                </li>
+                        </button>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+         <div class="navbar-collapse">
+            <ul class="nav navbar-nav mr-auto">
             </ul>
-            <!-- Right(Notification) -->
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown user-box">
-                    <a href="" class="dropdown-toggle waves-effect user-link" data-toggle="dropdown" aria-expanded="true">
-                        <img src="assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle user-img">
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right user-list notify-list">
-                        <li>
-                            <h5>Hi, Admin</h5>
+            <ul class="nav nav-item hidden-sm-up">
+               <li class="dropdown user-box">
+                  <a href="" class="dropdown-toggle waves-effect user-link" data-toggle="dropdown" aria-expanded="true">
+                  <img src="assets/images/usericon.png" alt="user-img" class="img-circle user-img">
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right user-list notify-list" style="padding: 0px 0px 0px 0px;">
+                     <span class="with-arrow">
+                     <span class="bg-primary"></span>
+                     </span>
+                     <div class="d-flex no-block align-items-center p-15 bg-pf text-white m-b-10" style="height: 100px;width: 248px;">
+                        <li class="block-pf"> 
+                           <img src="assets/images/usericon.png" alt="user-img" class="img-circle user-img">
                         </li>
-                        <li><a href="change-password.php"><i class="ti-settings m-r-5"></i> Chnage Password</a></li>
-                        <li><a href="logout.php"><i class="ti-power-off m-r-5"></i> Logout</a></li>
-                    </ul>
-                </li>
-            </ul> <!-- end navbar-right -->
-        </div><!-- end container -->
-    </div><!-- end navbar -->
+                        <div class="m-l-10 pf-name">
+                           <h4 class="m-b-0">Steave Jobs</h4>
+                           <p class=" m-b-0">varun@gmail.com</p>
+                        </div>
+                     </div>
+                     <!-- <li>
+                        <h5>Hi, Admin</h5>
+                        </li> -->
+                     <li><a href=""><i class="ti-user icon-size"></i> My profile </a></li>
+                     <li><a href="change-password.php"><i class="ti-settings icon-size"></i> Chnage Password</a></li>
+                     <div class="dropdown-divider"></div>
+                     <li><a href="logout.php"><i class="ti-power-off icon-size"></i> Logout</a></li>
+                  </ul>
+               </li>
+            </ul>
+            <ul class="nav">
+               <li class="nav-item right-side-toggle">
+                  <a class="nav-link  waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a>
+                  <!-- <div id="mySidenav" class="sidenav">
+                     <a class="nav-link  waves-effect waves-light" href="javascript:void(0)" onclick="closeNav()"><i class="ti-settings"></i></a>
+                     </div> -->
+               </li>
+            </ul>
+         </div>
+      </nav>
+   </header>
+   <aside class="left-sidebar" style="left: 0;width: 100%; position: fixed;">
+      <div class="navbar-header">
+         <div class="d-flex no-block nav-text-box align-items-center">
+            <a class="navbar-brand" href="index.html">
+            <span>NEWS<span>Top</span></span><i class="mdi mdi-layers"></i>
+            </a>
+            <!-- <a class="navbar-brand" href="index.html"> -->
+            <!-- Logo icon -->
+            <b>
+               <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+               <!-- Dark Logo icon -->
+               <!-- <img src="assets/images/logo.png" alt="" height="30"> -->
+               <!-- Light Logo icon -->
+               <!-- <img src="../assets/images/logo-light-icon.png" alt="homepage" class="light-logo"> -->
+            </b>
+            <!--End Logo icon -->
+            <!-- Logo text --><span>
+            <!-- dark Logo text -->
+            <!-- <img src="../assets/images/logo-text.png" alt="homepage" class="dark-logo"> -->
+            <!-- Light Logo text -->    
+            <!-- <img src="../assets/images/logo-light-text.png" class="light-logo" alt="homepage"></span>  -->
+            </a>
+         </div>
+      </div>
+   </aside>
 </div>
--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,22 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','news');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-// Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+// define('DB_SERVER','localhost');
+// define('DB_USER','root');
+// define('DB_PASS' ,'');
+// define('DB_NAME','news');
+// $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// // Check connection
+// if (mysqli_connect_errno())
+// {
+//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// }
+
+		$servername = "127.0.0.1";
+		$username = "root";
+		$password = "12345";
+		$dbname = "news";
+		$con = new mysqli($servername, $username, $password, $dbname);
+		if ($con->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+		
 ?>
